a409e938b31bcd331144a6f214aec24e91bc338a
doudou <doudoud82@gmail.com>
2026-07-15 21:23:06 +0100

    add post install service to install applications while user is doing User creation.

--- build_files/build.sh
+++ build_files/build.sh
@@ -106,5 +106,7 @@ find /usr/share/gnome-shell/extensions/systemd-manager@hardpixel.eu -type f -exe
 glib-compile-schemas /usr/share/gnome-shell/extensions/systemd-manager@hardpixel.eu/schemas
 
 rm -f "$tmp"
+#Cursor Icon
+wget "https://uxwing.com/wp-admin/admin-ajax.php?action=resize_image&size=128x128&file=cursor-ai-code-icon.png&category_slug=brands-and-social-media" -O "/usr/share/icons/cursor.png"
 
 dconf update

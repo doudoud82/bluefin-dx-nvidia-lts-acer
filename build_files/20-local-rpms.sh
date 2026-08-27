e7b2045990d33b0511ccd395006eaa51f4ff054d
doudou <doudoud82@gmail.com>
2026-08-27 22:39:27 +0100

    Switch windscribe to be downloaded via wget from source.

--- build_files/20-local-rpms.sh
+++ build_files/20-local-rpms.sh
@@ -4,5 +4,6 @@ set -ouex pipefail
 
 mkdir -p /var/usrlocal/bin
 mkdir -p /var/opt
-dnf install -y --no-gpgchecks --setopt=tsflags=nocrypto /ctx/local_rpms/*.rpm
+wget https://windscribe.com/install/desktop/linux_rpm_x64 -O /tmp/windscribe.rpm
+dnf install -y --no-gpgchecks --setopt=tsflags=nocrypto /tmp/windscribe.rpm /ctx/local_rpms/*.rpm
 dnf clean all

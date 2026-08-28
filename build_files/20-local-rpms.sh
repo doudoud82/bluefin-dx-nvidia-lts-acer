0e19a7d45b7ebf5fde2ee2ded9a3418648933399
doudou <doudoud82@gmail.com>
2026-08-28 22:41:25 +0100

    Change local rpms to be installed in the container. Did some symlink tricks to have windscribe and capt files live outside of /opt and /usr/local respectively, and make use of tmpfiles.d to symlink them on the booted system

--- build_files/20-local-rpms.sh
+++ build_files/20-local-rpms.sh
@@ -2,9 +2,23 @@
 
 set -ouex pipefail
 
-mkdir /rpms
+mkdir -p /tmp/rpm
 
-wget https://windscribe.com/install/desktop/linux_rpm_x64 -O /rpms/windscribe.rpm
-wget https://www.hamrick.com/files/vuex6498.rpm -O /rpms/vuescan.rpm
+wget https://windscribe.com/install/desktop/linux_rpm_x64 -O /tmp/rpm/windscribe.rpm
+wget https://www.hamrick.com/files/vuex6498.rpm -O /tmp/rpm/vuescan.rpm
+mkdir -p /var/opt
+# Windscribe
+mkdir -p /usr/lib/windscribe
+ln -s /usr/lib/windscribe /opt/windscribe
 
-cp /ctx/local_rpms/*.rpm /rpms
+# Capt
+mkdir -p /var/usrlocal
+mkdir -p /usr/lib/local-overlay/bin
+mkdir -p /usr/lib/local-overlay/lib64
+mkdir -p /usr/lib/local-overlay/share/locale
+ln -s /usr/lib/local-overlay/bin /usr/local/bin
+ln -s /usr/lib/local-overlay/lib64 /usr/local/lib64
+mkdir -p /usr/local/share
+ln -s /usr/lib/local-overlay/share/locale /usr/local/share/locale
+
+dnf install -y --no-gpgchecks --setopt=tsflags=nocrypto /ctx/local_rpms/*.rpm /tmp/rpm/*.rpm

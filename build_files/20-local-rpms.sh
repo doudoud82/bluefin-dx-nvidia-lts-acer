c3332bb9bf608f256bbb00a3f7dfd468017c9d47
doudou <doudoud82@gmail.com>
2026-08-29 00:45:12 +0100

    fix uid/gid for windscribe.

--- build_files/20-local-rpms.sh
+++ build_files/20-local-rpms.sh
@@ -10,6 +10,14 @@ mkdir -p /var/opt
 # Windscribe
 mkdir -p /usr/lib/windscribe
 ln -s /usr/lib/windscribe /opt/windscribe
+groupadd -r windscribe
+useradd -r -g windscribe -d /var/home/windscribe -s /bin/false windscribe
+WS_UID="$(id -u windscribe)"
+WS_GID="$(id -g windscribe)"
+cat > /usr/lib/sysusers.d/windscribe.conf <<EOF
+g windscribe $WS_GID
+u windscribe $WS_UID:$WS_GID "windscribe" /var/home/windscribe /bin/false
+EOF
 
 # Capt
 mkdir -p /var/usrlocal
@@ -22,3 +30,6 @@ mkdir -p /usr/local/share
 ln -s /usr/lib/local-overlay/share/locale /usr/local/share/locale
 
 dnf install -y --no-gpgchecks --setopt=tsflags=nocrypto /ctx/local_rpms/*.rpm /tmp/rpm/*.rpm
+
+sed -i '/^windscribe:/d' /etc/passwd
+sed -i '/^windscribe:/d' /etc/group

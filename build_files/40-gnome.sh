5ab458d1784e85772cef110a00ec32d5ba86a81c
doudou <doudoud82@gmail.com>
2026-09-16 00:46:52 +0100

    add gnome browser connector

--- build_files/40-gnome.sh
+++ build_files/40-gnome.sh
@@ -13,7 +13,7 @@ curl_retry() {
 }
 
 dnf -y remove gnome-extensions-app 
-
+dnf -y gnome-browser-connector
 EXTENSIONS=(
     "adw-gtk3-colorizer@NiffirgkcaJ.github.com"
     "Bluetooth-Battery-Meter@maniacx.github.com"
